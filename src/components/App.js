421932621d7dc0ec0a035041c8b585c34b78e4f7
isaac-martin <isaac@weareplayground.co>
2018-06-08 09:28:08 -0400

    added play pause button

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,59 @@
+/* global Mixcloud */
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import FeaturedMix from './FeaturedMix';
 import Header from './Header';
 
+const Home = () => <h1>Home</h1>;
+const Archive = () => <h1>Archive</h1>;
+const About = () => <h1>About</h1>;
+
 class App extends Component {
+  mountAudio = async () => {
+    this.widget = Mixcloud.PlayerWidget(this.player);
+    await this.widget.ready;
+    await this.widget.play();
+    console.log(this.widget);
+  };
+
+  componentDidMount() {
+    this.mountAudio();
+  }
+
+  togglePlay = () => {
+    console.log('toggleplay');
+    this.widget.togglePlay();
+  };
+
   render() {
     return (
-      <div>
-        <div className="flex-l justify-end">
-          {/* Featured Mix */}
-          <FeaturedMix />
-          <div className="w-50-l relative z-1">
-            <Header />
-            {/* Routed page*/}
+      <Router>
+        <div>
+          <div className="flex-l justify-end">
+            {/* Featured Mix */}
+            <FeaturedMix />
+            <div className="w-50-l relative z-1">
+              <Header />
+              {/* Routed page*/}
+
+              <div>
+                <button onClick={this.togglePlay}>Play/Pause</button>
+              </div>
+              <Route exact path="/" component={Home} />
+              <Route path="/archive" component={Archive} />
+              <Route path="/about" component={About} />
+            </div>
+            <iframe
+              className="player db fixed bottom-0 z-5"
+              width="100%"
+              height="60"
+              src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
+              frameBorder="0"
+              ref={player => (this.player = player)}
+            />
           </div>
-          <iframe
-            className="player db fixed bottom-0 z-5"
-            width="100%"
-            height="60"
-            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
-            frameBorder="0"
-          />
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
-
+import {NavLink} from 'react-router-dom';
 const Header = props => (
   <header className="black mb5 pt5">
     <h1 className="ttu f3 tracked-mega anton tc mt0 mb3">Marmalade.fm</h1>
     <ul className="list flex justify-center pl0">
       <li className="mh2">
-        <a className="nav-link f6 ttu biryani-black gray">Whats Hot</a>
+        <NavLink exact to="/" className="nav-link f6 ttu biryani-black gray">
+          Whats Hot
+        </NavLink>
       </li>
       <li className="mh2">
-        <a className="nav-link f6 ttu biryani-black gray">Archive</a>
+        <NavLink to="/archive" className="nav-link f6 ttu biryani-black gray">
+          Archive
+        </NavLink>
       </li>
       <li className="mh2">
-        <a className="nav-link f6 ttu biryani-black gray">About</a>
+        <NavLink to="/about" className="nav-link f6 ttu biryani-black gray">
+          About
+        </NavLink>
       </li>
     </ul>
   </header>
