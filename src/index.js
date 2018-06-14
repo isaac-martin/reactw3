358e9c91e277565a7d6dbd32fee883d88bd6604d
isaac-martin <isaac@weareplayground.co>
2018-06-14 09:21:20 -0400

    Added Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import 'tachyons';
 import './css/main.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import mixesApp from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(
+  mixesApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
